287f0a9822e7ef9a0aeb457210ebeffad91ef485
soraqaharis <149798211+soraqaharis@users.noreply.github.com>
2024-07-08 23:06:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 mongoose.connect('mongodb://localhost:27017/add', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    uuseNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Failed to connect to MongoDB', err));
